75ae07831d244c9d568b854532c82bf922b7e533
Bugra Kalayci <kalaycibugra@gmail.com>
2019-02-11 14:01:30 -0500

    Swipe Gesture Detection Handled

--- app/src/main/java/com/example/my2048/game.java
+++ app/src/main/java/com/example/my2048/game.java
@@ -2,13 +2,33 @@ package com.example.my2048;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.MotionEvent;
+import android.view.View;
+import android.app.Activity;
+import android.widget.TextView;
+import android.widget.LinearLayout;
 public class game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        LinearLayout board1 =(LinearLayout) findViewById(R.id.board);
+        final TextView in=(TextView) findViewById(R.id.index00);
+        board1.setOnTouchListener(new OnSwipeTouchListener(game.this){
+        public void onSwipeTop() {
+            in.setText("245");
+        }
+        public void onSwipeRight() {
+            in.setText("244");
+        }
+        public void onSwipeLeft() {
+            in.setText("243");
+        }
+        public void onSwipeBottom() {
+            in.setText("242");
+        }
+    });
 
-    }
-}
+
+}}
